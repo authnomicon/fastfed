f09ceb131a2db16d5638ce7d7eda1e1a9ddaefa6
Jared Hanson <jaredhanson@gmail.com>
2018-03-21 13:49:06 -0700

    Add app consent endpoints.

---
+++ app/http/consent/idp/service.js
@@ -0,0 +1,25 @@
+// has start and finish
+
+exports = module.exports = function(applicationPromptHandler, applicationDecisionHandler) {
+  var express = require('express');
+  var router = new express.Router();
+  
+  router.get('/application', applicationPromptHandler);
+  router.post('/application', applicationDecisionHandler);
+  
+  // TODO: consent for initiating FastFed and releasing tenant data
+  //router.get('/handshake');
+  //router.post('/handshake');
+  
+  return router;
+};
+
+exports['@implements'] = [
+  'http://i.bixbyjs.org/http/Service',
+  'http://schemas.authnomicon.org/js/http/fastfed/IdPConsentService'
+];
+exports['@path'] = '/fastfed/consent';
+exports['@require'] = [
+  './handlers/application/prompt',
+  './handlers/application/decision'
+];
