c7e03c44963fb9b46198bddb70290f729337d788
Jared Hanson <jaredhanson@gmail.com>
2018-04-20 10:20:56 -0700

    Move prompt to ceremony.

--- app/http/idp/setup/ceremony.js
+++ app/http/idp/setup/ceremony.js
@@ -1,12 +1,14 @@
-exports = module.exports = function(spawn) {
+exports = module.exports = function(spawn, prompt) {
   
   return {
     spawn: spawn,
+    prompt: prompt
   };
 };
 
 exports['@implements'] = 'http://i.bixbyjs.org/http/ceremony/Prompt';
 exports['@name'] = 'fastfed-enable-application';
 exports['@require'] = [
-  './ceremony/spawn'
+  './ceremony/spawn',
+  './ceremony/prompt'
 ];
--- app/http/idp/setup/handlers/prompt.js
+++ app/http/idp/setup/handlers/prompt.js
@@ -1,44 +1,11 @@
-exports = module.exports = function(ceremony, initialize, authenticate, errorLogging) {
-  var path = require('path')
-    , ejs = require('ejs')
-  
-  
-  function render(req, res, next) {
-    //console.log('FASTFED CONSENT FOR APP ENABLE?');
-    //console.log(req.query);
-    //console.log(req.state);
-    
-    
-    // TODO: Load this based on query param
-    res.locals.application = { displayName: 'Foo' };
-    
-    res.render('fastfed/setup/application', function(err, str) {
-      if (err && err.view) {
-        var view = path.resolve(__dirname, '../views/prompt.ejs');
-        ejs.renderFile(view, res.locals, function(err, str) {
-          if (err) { return next(err); }
-          res.send(str);
-        });
-        return;
-      } else if (err) {
-        return next(err);
-      }
-      res.send(str);
-    });
-  }
-  
+exports = module.exports = function(authenticate, ceremony) {
   
   return ceremony('fastfed-enable-application',
-    authenticate([ 'session' ]),
-    initialize(),
-    render,
-    errorLogging()
+    authenticate([ 'session' ])
   );
 };
 
 exports['@require'] = [
-  'http://i.bixbyjs.org/http/middleware/ceremony',
-  'http://i.bixbyjs.org/http/middleware/initialize',
   'http://i.bixbyjs.org/http/middleware/authenticate',
-  'http://i.bixbyjs.org/http/middleware/errorLogging'
+  'http://i.bixbyjs.org/http/middleware/ceremony'
 ];
