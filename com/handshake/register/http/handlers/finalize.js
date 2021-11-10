80499f078c47e0cf55340c45414f1c318466efe5
Jared Hanson <jaredhanson@gmail.com>
2021-11-10 11:11:08 -0800

    WIP

---
+++ com/handshake/register/http/handlers/finalize.js
@@ -0,0 +1,23 @@
+exports = module.exports = function(imd, md, ceremony, initialize, authenticate, errorLogging) {
+  
+  
+  // TODO: Authenticate the admin?  What if this is a first deployment, and you want
+  //      to use the IdP's accounts for login?
+  
+  function go(req, res, next) {
+    res.redirect('/ok');
+  }
+  
+  return [
+    go
+  ];
+};
+
+exports['@require'] = [
+  '../../../../instance-metadata/main',
+  '../../../../metadata/main',
+  'http://i.bixbyjs.org/http/middleware/ceremony',
+  'http://i.bixbyjs.org/http/middleware/initialize',
+  'http://i.bixbyjs.org/http/middleware/authenticate',
+  'http://i.bixbyjs.org/http/middleware/errorLogging'
+];
