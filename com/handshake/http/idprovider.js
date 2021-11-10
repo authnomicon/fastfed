d9c2b1e7c5fb18ba769d6a046b5eefe226058354
Jared Hanson <jaredhanson@gmail.com>
2021-11-10 13:27:26 -0800

    WIP

--- com/handshake/http/idprovider.js
+++ com/handshake/http/idprovider.js
@@ -1,16 +1,16 @@
-exports = module.exports = function(startHandler, finishHandler) {
+exports = module.exports = function(startHandler, finalizeHandler) {
   var express = require('express');
   var router = new express.Router();
   
   router.get('/start', startHandler);
-  router.get('/finish', finishHandler);
+  router.get('/finalize', finalizeHandler);
   
   return router;
 };
 
 exports['@implements'] = 'http://i.bixbyjs.org/http/Service';
-exports['@path'] = '/fastfed/start';  // /fastfed/handshake/start?
+exports['@path'] = '/fastfed';  // /fastfed/handshake/start?
 exports['@require'] = [
   './handlers/start',
-  './handlers/finish'
+  './handlers/finalize'
 ];
